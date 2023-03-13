32f595192c5d4db901699dfa9fd41d802139b7be
dooco <padraigdooley@gmail.com>
2023-03-13 21:22:13 +0000

    fix js min cdn on base.html

--- bag/views.py
+++ bag/views.py
@@ -63,9 +63,9 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """ Remove the product from the cart """
+    """Remove the item from the shopping bag"""
+
     try:
-        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -75,16 +75,11 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, 
-            (f'Removed size {size.upper()}'
-            f'{product.name} from your bag'))
         else:
             bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
